f0375bc27ab0d7f2ce3ea9ca8946f9f245c471d8
Hjwad <158073869+Hjwad@users.noreply.github.com>
2024-04-05 14:02:36 +0300

    Update loop.py

--- Auput/plugins/admins/loop.py
+++ Auput/plugins/admins/loop.py
@@ -17,6 +17,12 @@ LOOP_COMMAND = get_command("LOOP_COMMAND")
     & filters.group
     & ~BANNED_USERS
 )
+
+@app.on_message(filters.command(["تكرار"],"")
+& filters.group
+    & ~BANNED_USERS
+)
+
 @AdminRightsCheck
 async def admins(cli, message: Message, _, chat_id):
     usage = _["admin_24"]
