be422a06de58113759b315d55827444720a3654b
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-05-13 05:47:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -59,6 +59,8 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
     "SUPPORT_CHANNEL", "https://t.me/mmmsc")
