66e00004ee3a386f75ba0872554d38a6fe6be96d
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-05-17 20:06:14 +0300

    Update shuffle.py

--- Auput/plugins/admins/shuffle.py
+++ Auput/plugins/admins/shuffle.py
@@ -16,7 +16,7 @@ SHUFFLE_COMMAND = get_command("SHUFFLE_COMMAND")
 
 
 @app.on_message(
-    filters.command(SHUFFLE_COMMAND)
+    command(SHUFFLE_COMMAND)
     & filters.group
     & ~BANNED_USERS
 )
