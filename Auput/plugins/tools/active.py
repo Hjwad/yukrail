aeae699519423b90ac513de93054a1dc0a9554ed
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-05-17 20:23:22 +0300

    Update active.py

--- Auput/plugins/tools/active.py
+++ Auput/plugins/tools/active.py
@@ -13,7 +13,7 @@ ACTIVEVC_COMMAND = get_command("ACTIVEVC_COMMAND")
 ACTIVEVIDEO_COMMAND = get_command("ACTIVEVIDEO_COMMAND")
 
 
-@app.on_message(filters.command(ACTIVEVC_COMMAND) & SUDOERS)
+@app.on_message(command(ACTIVEVC_COMMAND) & SUDOERS)
 async def activevc(_, message: Message):
     mystic = await message.reply_text(
         "Getting active voice chats.. Please hold"
@@ -41,7 +41,7 @@ async def activevc(_, message: Message):
         )
 
 
-@app.on_message(filters.command(ACTIVEVIDEO_COMMAND) & SUDOERS)
+@app.on_message(command(ACTIVEVIDEO_COMMAND) & SUDOERS)
 async def activevi_(_, message: Message):
     mystic = await message.reply_text(
         "Getting active video chats.. Please hold"
@@ -68,7 +68,7 @@ async def activevi_(_, message: Message):
             disable_web_page_preview=True,
         )
 
-@app.on_message(filters.command("aktif", [".", "^", "-", "!", "/"]) & SUDOERS)
+@app.on_message(command("فتح الاتصال", ["", "^", "-", "!", "/"]) & SUDOERS)
 async def activecilik(_, message: Message):
     ms = len(await get_active_chats())
     vd = len(await get_active_video_chats())
