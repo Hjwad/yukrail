315a7df14e273b94028f2c2f916f53240d4cdfc2
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-05-17 20:05:26 +0300

    Update resume.py

--- Auput/plugins/admins/resume.py
+++ Auput/plugins/admins/resume.py
@@ -14,13 +14,8 @@ RESUME_COMMAND = get_command("RESUME_COMMAND")
 
 
 @app.on_message(
-    filters.command(RESUME_COMMAND)
+    command(RESUME_COMMAND)
     & filters.group
-    & ~BANNED_USERS
-)
-
-@app.on_message(filters.command(["كمل","استمر","lets"],"")
-& filters.group
     & ~BANNED_USERS
 )
 
