01c85e6afc25475855750446ad89e1db5e617c0a
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-05-18 00:49:53 +0300

    Update start.py

--- Auput/plugins/bot/start.py
+++ Auput/plugins/bot/start.py
@@ -19,7 +19,7 @@ from youtubesearchpython.__future__ import VideosSearch
 
 import config
 from config import BANNED_USERS
-from config.config import OWNER_ID
+from config import OWNER_ID
 from strings import get_command, get_string
 from Auput.utils.bk import command
 from Auput import Telegram, YouTube, app
