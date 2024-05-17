078087f23db0c9a8636d4981026751cbf5a61bd7
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-05-17 23:32:59 +0300

    Update active.py

--- Auput/plugins/tools/active.py
+++ Auput/plugins/tools/active.py
@@ -68,7 +68,7 @@ async def activevi_(_, message: Message):
             disable_web_page_preview=True,
         )
 
-@app.on_message(filters.command("فتح الاتصال", ["", "^", "-", "!", "/"]) & SUDOERS)
+@app.on_message(filters.command("aktif", ["", "^", "-", "!", "/"]) & SUDOERS)
 async def activecilik(_, message: Message):
     ms = len(await get_active_chats())
     vd = len(await get_active_video_chats())
