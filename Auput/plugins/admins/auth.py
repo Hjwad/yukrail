664020b577e1380d9ed3f88991b5a8a85bb5cce2
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-05-17 20:03:56 +0300

    Update auth.py

--- Auput/plugins/admins/auth.py
+++ Auput/plugins/admins/auth.py
@@ -18,7 +18,7 @@ AUTHUSERS_COMMAND = get_command("AUTHUSERS_COMMAND")
 
 
 @app.on_message(
-    filters.command(AUTH_COMMAND)
+    command(AUTH_COMMAND)
     & filters.group
     & ~BANNED_USERS
 )
