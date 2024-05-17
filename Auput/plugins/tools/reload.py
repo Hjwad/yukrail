3f32b8c313f3e26389ec8adf20363d32f15cc9af
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-05-17 20:28:27 +0300

    Update reload.py

--- Auput/plugins/tools/reload.py
+++ Auput/plugins/tools/reload.py
@@ -21,7 +21,7 @@ RESTART_COMMAND = get_command("RESTART_COMMAND")
 
 
 @app.on_message(
-    filters.command(RELOAD_COMMAND)
+    command(RELOAD_COMMAND)
     & filters.group
     & ~BANNED_USERS
 )
@@ -48,7 +48,7 @@ async def reload_admin_cache(client, message: Message, _):
 
 
 @app.on_message(
-    filters.command(RESTART_COMMAND)
+    command(RESTART_COMMAND)
     & filters.group
     & ~BANNED_USERS
 )
