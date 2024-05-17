22e47b745a497612358a8ac7f77ea9bf39b11e22
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-05-17 20:19:37 +0300

    Update blacklistchat.py

--- Auput/plugins/sudo/blacklistchat.py
+++ Auput/plugins/sudo/blacklistchat.py
@@ -18,7 +18,7 @@ WHITELISTCHAT_COMMAND = get_command("WHITELISTCHAT_COMMAND")
 BLACKLISTEDCHAT_COMMAND = get_command("BLACKLISTEDCHAT_COMMAND")
 
 
-@app.on_message(filters.command(BLACKLISTCHAT_COMMAND) & SUDOERS)
+@app.on_message(command(BLACKLISTCHAT_COMMAND) & SUDOERS)
 @language
 async def blacklist_chat_func(client, message: Message, _):
     if len(message.command) != 2:
@@ -37,7 +37,7 @@ async def blacklist_chat_func(client, message: Message, _):
         pass
 
 
-@app.on_message(filters.command(WHITELISTCHAT_COMMAND) & SUDOERS)
+@app.on_message(command(WHITELISTCHAT_COMMAND) & SUDOERS)
 @language
 async def white_funciton(client, message: Message, _):
     if len(message.command) != 2:
@@ -52,7 +52,7 @@ async def white_funciton(client, message: Message, _):
 
 
 @app.on_message(
-    filters.command(BLACKLISTEDCHAT_COMMAND) & ~BANNED_USERS
+    command(BLACKLISTEDCHAT_COMMAND) & ~BANNED_USERS
 )
 @language
 async def all_chats(client, message: Message, _):
