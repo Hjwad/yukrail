37170d2ceafdc937364ccc1fd1acf111cde7469a
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-05-23 02:02:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -162,19 +162,19 @@ autoclean = []
 
 
 # Images
-START_IMG_URL = "https://telegra.ph/file/1bccb452031ac902596bf.jpg"
-PING_IMG_URL = "https://telegra.ph/file/1bccb452031ac902596bf.jpg"
-PLAYLIST_IMG_URL = "https://telegra.ph/file/1bccb452031ac902596bf.jpg"
-GLOBAL_IMG_URL = "https://telegra.ph/file/1bccb452031ac902596bf.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/1bccb452031ac902596bf.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/1bccb452031ac902596bf.jpg"
-TELEGRAM_VIDEO_URL = "https://telegra.ph/file/1bccb452031ac902596bf.jpg"
-STREAM_IMG_URL = "https://telegra.ph/file/1bccb452031ac902596bf.jpg"
-SOUNCLOUD_IMG_URL = "https://telegra.ph/file/1bccb452031ac902596bf.jpg"
-YOUTUBE_IMG_URL = "https://telegra.ph/file/1bccb452031ac902596bf.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/1bccb452031ac902596bf.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/1bccb452031ac902596bf.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/1bccb452031ac902596bf.jpg"
+START_IMG_URL = "https://telegra.ph/file/b13798d4d24ebeff2df35.jpg"
+PING_IMG_URL = "https://telegra.ph/file/b13798d4d24ebeff2df35.jpg"
+PLAYLIST_IMG_URL = "https://telegra.ph/file/b13798d4d24ebeff2df35.jpg"
+GLOBAL_IMG_URL = "https://telegra.ph/file/b13798d4d24ebeff2df35.jpg"
+STATS_IMG_URL = "https://telegra.ph/file/b13798d4d24ebeff2df35.jpg"
+TELEGRAM_AUDIO_URL = "https://telegra.ph/file/b13798d4d24ebeff2df35.jpg"
+TELEGRAM_VIDEO_URL = "https://telegra.ph/file/b13798d4d24ebeff2df35.jpg"
+STREAM_IMG_URL = "https://telegra.ph/file/b13798d4d24ebeff2df35.jpg"
+SOUNCLOUD_IMG_URL = "https://telegra.ph/file/b13798d4d24ebeff2df35.jpg"
+YOUTUBE_IMG_URL = "https://telegra.ph/file/b13798d4d24ebeff2df35.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/b13798d4d24ebeff2df35.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/b13798d4d24ebeff2df35.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/b13798d4d24ebeff2df35.jpg"
 
 
 def time_to_seconds(time):
