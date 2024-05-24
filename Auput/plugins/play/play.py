956d2f6e6501998bfbe31b8b727c869b822f4851
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-05-24 04:04:27 +0300

    Update play.py

--- Auput/plugins/play/play.py
+++ Auput/plugins/play/play.py
@@ -389,14 +389,14 @@ async def play_commnd(
             except NoActiveGroupCall:
                 await mystic.edit_text(_["black_9"])
                 return await app.send_message(
-                    chat_id=config.LOGGER_ID,
+                    chat_id=config.LOG_GROUP_ID,
                     text=_["play_17"],
                 )
             except Exception as e:
                 if "phone.CreateGroupCall" in str(e):
                     await mystic.edit_text(_["black_9"])
                     return await app.send_message(
-                        chat_id=config.LOGGER_ID,
+                        chat_id=config.LOG_GROUP_ID,
                         text=_["play_17"],
                     )
                 else:
