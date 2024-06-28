bbaaa347bc1f7f0407f5b80db4403fa3ea703d7e
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-06-28 17:50:56 +0300

    Update playy.py

--- Auput/utils/decorators/playy.py
+++ Auput/utils/decorators/playy.py
@@ -1,9 +1,9 @@
 #
-# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
+# Copyright (C) 2021-present by TeamAuput@Github, < https://github.com/TeamAuput >.
 #
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# This file is part of < https://github.com/TeamAuput/AuputBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+# Please see < https://github.com/TeamAuput/AuputBot/blob/master/LICENSE >
 #
 # All rights reserved.
 #
@@ -12,15 +12,15 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from config import PLAYLIST_IMG_URL, PRIVATE_BOT_MODE, adminlist
 from strings import get_string
-from YukkiMusic import YouTube, app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import (get_cmode, get_lang,
+from Auput import YouTube, app
+from Auput.misc import SUDOERS
+from Auput.utils.database import (get_cmode, get_lang,
                                        get_playmode, get_playtype,
                                        is_active_chat,
                                        is_commanddelete_on,
                                        is_served_private_chat)
-from YukkiMusic.utils.database.memorydatabase import is_maintenance
-from YukkiMusic.utils.inline.playlist import botplaylist_markup
+from Auput.utils.database.memorydatabase import is_maintenance
+from Auput.utils.inline.playlist import botplaylist_markup
 
 
 def PlayWrapper(command):
